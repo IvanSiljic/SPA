06905262a437592d102356a4d2df938216cf526e
isiljic <ivan.siljic.sspzo@gmail.com>
2023-11-10 23:22:06 +0100

    Zadatal 2

--- Zadaca/Zadaca4/zad2.cpp
+++ Zadaca/Zadaca4/zad2.cpp
@@ -3,17 +3,34 @@
 
 using namespace std;
 
-void radixSort(vector<int> &list) {
-    
+void countingSort(vector<int> &list) {
+    int max = list.at(0);
+
+    for (int i = 1; i < list.size(); i++) {
+        if (max < list.at(i))
+            max = list.at(i);
+    }
+
+    vector<vector<int>> countList{max + 1};
+
+    for (auto n : list)
+        countList.at(n).push_back(n);
+
+    list.clear();
+
+    for (auto i : countList) {
+        for (auto j : i)
+            list.push_back(j);
+    }
 }
 
 int main() {
     vector<int> list = {1, 2, 5, 2, 7, 3, 8, 1};
 
-    radixSort(list);
+    countingSort(list);
 
-    for (int i = 0; i < list.size(); i++) {
-        cout << list.at(i) << " ";
+    for (auto n : list) {
+        cout << n << " ";
     }
     cout << endl;
 
